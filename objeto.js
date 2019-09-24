716785f92e102671ee1777c0546a6af89d63bbef
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-09-23 22:39:07 -0300

    Aulas Cod3r 23/09/2019

---
+++ objeto.js
@@ -0,0 +1,18 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.80
+prod1['Desconto Legal'] = 0.40 //evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    valor: 99.30
+}
+
+console.log(prod2)
+
+let a = 3
+let b = a
+b++
+console.log(a, b) 
