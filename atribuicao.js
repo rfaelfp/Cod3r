af710533c0413e598f9a293d57c8366cff3a4910
Rafael Ferreira <rafaelfepedrosa@gmail.com>
2019-09-24 22:28:35 -0300

    Aulas 24/09/2019

---
+++ atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a //atribuição aditiva - b = b + a
+console.log(b)
+
+b -= 4 //atriubuição subtrativa - b = b - 4
+console.log(b)
+
+b *= 2 // atribuição multiplicação - b = b * 2
+console.log(b)
+
+b /= 2
+console.log(b)
+
+b %= 2 // atribuição modular. O resto define o valor 0 ou 1 sendo 0 = par e 1 = impar
+console.log(b)
