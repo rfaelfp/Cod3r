5cbbcf5486e56d0187f0444cacf9e1275637cb8a
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-05-07 14:54:09 -0300

    07/05/2020

--- Função/thisEBind2.js
+++ Função/thisEBind2.js
@@ -1,12 +1,13 @@
- function Pessoa() {
+function Pessoa() {
     this.idade = 0
 
     const self = this // tecnica para driblar o NaN
-    setInterval(function() { // dispara outra função a partir de um intervalo que passar 
+    setInterval(function () { // dispara outra função a partir de um intervalo que passar 
         self.idade++
         console.log(self.idade)
     }/*.bind(this)*/, 1000) // o 1000 representa milisegundos, ou seja a cada 1000 milisegundos a função será disparada
-                            // tem que utilizar o bind para referenciar o this e a função setInterval utilizar a variável idade
- }
+    // tem que utilizar o bind para referenciar o this e a função setInterval utilizar a variável idade
+}
 
- new Pessoa
+new Pessoa
+
