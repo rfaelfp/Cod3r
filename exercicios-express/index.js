bc4f6f57b0b0412c004e77d79b6f134e6fd7b8c6
rfalefp <rafaelfepedrosa@gmail.com>
2021-10-27 21:55:51 -0300

    27/10/2021

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,16 +1,52 @@
-const express = require('express')
-const app = express()
+const { application } = require("express");
+const express = require("express");
+const app = express();
 
-app.use('/opa', (req, res) => {
-    res.json({
-        nome: "ipad 32gb",
-        price: "1899.99",
-        discount: 0.15
-    })
-})
+const bodyParser = require("body-parser");
 
+const saudacao = require("./saudacaoMid");
+const usuarioApi = require("./api/usuario");
+require("./api/produto")(app, "com param!");
 
+app.post("/usuario", usuarioApi.salvar);
+app.get("/usuario", usuarioApi.obter);
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(saudacao("Guilherme"));
+
+app.use("/opa", (req, res) => {
+  res.json({
+    nome: "ipad 32gb",
+    price: "1899.99",
+    discount: 0.15,
+  });
+});
+
+app.get("/clientes/relatorio", (req, res) => {
+  res.send(
+    `Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`
+  );
+});
+
+app.post("/corpo", (req, res) => {
+  // let corpo = ''
+  // req.on('data', function(parte) {
+  //     corpo += parte
+  // })
+
+  // req.on('end', function() {
+  //     res.send(corpo)
+  // })
+  res.send(req.body);
+});
+
+app.get("/clientes/:id", (req, res) => {
+  res.send(`Cliente ${req.params.id} selecionado!`);
+});
 
 app.listen(3000, () => {
-    console.log('Backend executando')
-})
+  console.log("Backend executando");
+});
